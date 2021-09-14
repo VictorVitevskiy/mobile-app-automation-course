610d0150a3e16813047843a5e1f267efc694ab76
Victor Vitevskiy <vitevskiy@mail.ru>
2021-09-14 10:56:01 +0300

    Fourth lesson. First task.

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -533,7 +533,165 @@ public class FirstTest {
         );
     }
 
+    @Test
+    public void saveTwoArticlesToMyList() {
 
+        String folder_name = "Learning programming";
+        String first_title = "Java (programming language)";
+        String second_title = "Python (programming language)";
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia' input",
+                15
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Java",
+                "Cannot find search input",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='" + first_title + "']"),
+                "Cannot find '" + first_title + "' title",
+                30
+        );
+
+        waitForElementAndClick(
+                By.id("More options"),
+                "Cannot find button to open article options",
+                10
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Add to reading list')]"),
+                "Cannot find option to add article to reading list",
+                10
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/onboarding_button"),
+                "Cannot find 'Got it' tip overlay",
+                10
+        );
+
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/text_input"),
+                "Cannot find input to set name of article folder",
+                10
+        );
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/text_input"),
+                folder_name,
+                "Cannot put text into article folder input",
+                10
+        );
+
+        waitForElementAndClick(
+                By.id("android:id/button1"),
+                "Cannot press OK button",
+                10
+        );
+
+        waitForElementAndClick(
+                By.id("Navigate up"),
+                "Cannot close article, cannot find X button",
+                10
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia' input",
+                15
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Python",
+                "Cannot find search input",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='" + second_title + "']"),
+                "Cannot find '"+ second_title + "' title",
+                30
+        );
+
+        waitForElementAndClick(
+                By.id("More options"),
+                "Cannot find button to open article options",
+                10
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Add to reading list')]"),
+                "Cannot find option to add article to reading list",
+                10
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text='" + folder_name + "']"),
+                "Cannot find option to add article to My lists",
+                10
+        );
+
+        waitForElementAndClick(
+                By.id("Navigate up"),
+                "Cannot close article, cannot find X button",
+                10
+        );
+
+        waitForElementAndClick(
+                By.id("My lists"),
+                "Cannot find navigation button to My lists",
+                10
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text='" + folder_name + "']"),
+                "Cannot find option to open my list",
+                10
+        );
+
+        swipeElementToLeft(
+                By.xpath("//*[@text='" + first_title + "']"),
+                "Cannot find saved article"
+        );
+
+        waitForElementNotPresent(
+                By.xpath("//*[@text='" + first_title + "']"),
+                "Cannot delete saved article",
+                10
+        );
+
+        waitForElementPresent(
+                By.xpath("//*[@text='" + second_title + "']"),
+                "Cannot find saved article",
+                10
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='" + second_title + "']"),
+                "Cannot find '" + second_title + "' title",
+                30
+        );
+
+        WebElement title_element = waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find saved article",
+                10
+        );
+
+        Assert.assertEquals(
+                "Article title do not match",
+                second_title,
+                title_element.getAttribute("text")
+        );
+    }
 
     private WebElement waitForElementPresent(By by, String error_message, long timeout_in_seconds) {
 
